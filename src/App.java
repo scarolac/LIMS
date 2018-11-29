6507e1ea3ca59c7f9dd6925ff3646b2316ba0287
Chris <scarolac@gmail.com>
2018-11-28 22:19:12 -0700

    comment out test, update app

--- src/App.java
+++ src/App.java
@@ -1,5 +1,7 @@
 
+import models.*;
 import views.*;
+import controllers.*;
 
 import jexer.*;
 
@@ -19,6 +21,28 @@ public class App extends TApplication {
         try {
             App app = new App();
             (new Thread(app)).start();
+
+            LibraryInformation libraryInformation = LibraryInformation.getInstance();
+            
+            Library libNorlin = new Library("Norlin");
+            Library libBusiness = new Library("Business");
+            Library libEngineering = new Library("Engineering");
+            Library libLaw = new Library("Law");
+            Library libMusic = new Library("Music");
+            libraryInformation.addLibrary(libNorlin);
+            libraryInformation.addLibrary(libBusiness);
+            libraryInformation.addLibrary(libEngineering);
+            libraryInformation.addLibrary(libLaw);
+            libraryInformation.addLibrary(libMusic);
+            libNorlin.addMedia(new Book("trees", "Norlin", "Me","ABC345","McGraw"));
+            Media m1 = new Book("trees", "Engineering", "Me","ABC345","McGraw");
+            libEngineering.addMedia(m1);
+        
+
+            Reader r1 = new Reader("Chris Scarola", "scarolac@colorado.edu");
+            r1.setFees(23.13);
+            libraryInformation.addReader(r1);
+
         } catch (Throwable t) {
             t.printStackTrace();
         }
--- src/AppTest.java
+++ src/AppTest.java
@@ -1,124 +1,124 @@
 
-import models.*;
-import views.*;
-import controllers.*;
+// import models.*;
+// import views.*;
+// import controllers.*;
 
-import java.util.*;
-import jexer.*;
+// import java.util.*;
+// import jexer.*;
 
-public class AppTest extends TApplication
-{
+// public class AppTest extends TApplication
+// {
 
-    public static <T> void print(T s)
-    {
-        System.out.println(s);
-    }
+//     public static <T> void print(T s)
+//     {
+//         System.out.println(s);
+//     }
 
-    public static void testOne()
-    {
-        Media m = new Book("trees", "Norlin", "Me","ABC345","McGraw");
-        // Media m2 = new Media("car", "place");
-        Reader r = new Reader();
-        Reader r2 = new Reader("joe", "no@no.com");
-        Librarian l = new Librarian("John", 0);
-        print(l.getName());
-        print(m.getTitle());
-        print(m.getLocation());
-        print(r.getEmail());
-        r.addFees(55.76);
-        print(r.getFees());
-        r.checkOutMedia(m);
-        m.checkOut(r);
-        r2.checkOutMedia(m);
-        m.checkOut(r2);
-        // r.checkOutMedia(m2);
-        print(r.getId());
-        print(r2.getId());
-        for(Media i : r.getCheckedOut())
-        {
-            print(i);
-        }
+//     public static void testOne()
+//     {
+//         Media m = new Book("trees", "Norlin", "Me","ABC345","McGraw");
+//         // Media m2 = new Media("car", "place");
+//         Reader r = new Reader();
+//         Reader r2 = new Reader("joe", "no@no.com");
+//         Librarian l = new Librarian("John", 0);
+//         print(l.getName());
+//         print(m.getTitle());
+//         print(m.getLocation());
+//         print(r.getEmail());
+//         r.addFees(55.76);
+//         print(r.getFees());
+//         r.checkOutMedia(m);
+//         m.checkOut(r);
+//         r2.checkOutMedia(m);
+//         m.checkOut(r2);
+//         // r.checkOutMedia(m2);
+//         print(r.getId());
+//         print(r2.getId());
+//         for(Media i : r.getCheckedOut())
+//         {
+//             print(i);
+//         }
 
-        for(Media i : r2.getCheckedOut())
-        {
-            print(i);
-        }
-    }
+//         for(Media i : r2.getCheckedOut())
+//         {
+//             print(i);
+//         }
+//     }
 
-    public static void testTwo()
-    {
-        LibraryInformation lI = LibraryInformation.getInstance();
-        Reader r = new Reader("Joe", "email");
-        lI.addReader(r);
-        print(lI.getReaders().get(0).getId());
-        lI.removeReader(1);
-    }
+//     public static void testTwo()
+//     {
+//         LibraryInformation lI = LibraryInformation.getInstance();
+//         Reader r = new Reader("Joe", "email");
+//         lI.addReader(r);
+//         print(lI.getReaders().get(0).getId());
+//         lI.removeReader(1);
+//     }
 
-    public static void testThree()
-    {
-        Library lib = new Library("Norlin");
-        Library libBusiness = new Library("Business");
-        Library libEngineering = new Library("Engineering");
-        Library libLaw = new Library("Law");
-        Library libMusic = new Library("Music");
+//     public static void testThree()
+//     {
+//         Library lib = new Library("Norlin");
+//         Library libBusiness = new Library("Business");
+//         Library libEngineering = new Library("Engineering");
+//         Library libLaw = new Library("Law");
+//         Library libMusic = new Library("Music");
 
-        String args2[] = new String[] {"trees", "Norlin", "Me","ABC345","McGraw"};
-        print(Arrays.toString(args2));
-        print(lib.addMedia(new Book(args2[0], args2[1], args2[2], args2[3], args2[4])));
-        lib.addMedia(new Book("trees", "Norlin", "Me","ABC345","McGraw"));
-        lib.addMedia(new Book("trees", "Norlin", "Me","ABC345","McGraw"));
-        lib.addMedia(new Book("cats", "Norlin", "Me","ABC345","McGraw"));
-        lib.addMedia(new Book("trees", "Norlin", "Me","ABC345","McGraw"));
+//         String args2[] = new String[] {"trees", "Norlin", "Me","ABC345","McGraw"};
+//         print(Arrays.toString(args2));
+//         print(lib.addMedia(new Book(args2[0], args2[1], args2[2], args2[3], args2[4])));
+//         lib.addMedia(new Book("trees", "Norlin", "Me","ABC345","McGraw"));
+//         lib.addMedia(new Book("trees", "Norlin", "Me","ABC345","McGraw"));
+//         lib.addMedia(new Book("cats", "Norlin", "Me","ABC345","McGraw"));
+//         lib.addMedia(new Book("trees", "Norlin", "Me","ABC345","McGraw"));
 
 
-        ArrayList<Media> temp = lib.searchMedia("trees", "Book");
-        // temp.asArray();
-        print(temp);
-    }
+//         ArrayList<Media> temp = lib.searchMedia("trees", "Book");
+//         // temp.asArray();
+//         print(temp);
+//     }
 
-    public AppTest() throws Exception {
-        super(BackendType.SWING); // Use Swing Frames instead of terminal
+//     public AppTest() throws Exception {
+//         super(BackendType.SWING); // Use Swing Frames instead of terminal
 
-        // Title for frame
-        getBackend().setTitle("LIMS");
+//         // Title for frame
+//         getBackend().setTitle("LIMS");
 
-        // Start on "login" page
-        new LoginScreen(this);      
-    }
+//         // Start on "login" page
+//         new LoginScreen(this);      
+//     }
 
-    public static void main(String[] args) 
-    {
-        // testOne();
-        // testTwo();
-        // testThree();
+//     public static void main(String[] args) 
+//     {
+//         // testOne();
+//         // testTwo();
+//         // testThree();
         
-        try 
-        {
-            AppTest app = new AppTest();
-            (new Thread(app)).start();
+//         try 
+//         {
+//             AppTest app = new AppTest();
+//             (new Thread(app)).start();
 
-            LibraryInformation libraryInformation = LibraryInformation.getInstance();
+//             LibraryInformation libraryInformation = LibraryInformation.getInstance();
             
-            Library libNorlin = new Library("Norlin");
-            Library libBusiness = new Library("Business");
-            Library libEngineering = new Library("Engineering");
-            Library libLaw = new Library("Law");
-            Library libMusic = new Library("Music");
-            libraryInformation.addLibrary(libNorlin);
-            libraryInformation.addLibrary(libBusiness);
-            libraryInformation.addLibrary(libEngineering);
-            libraryInformation.addLibrary(libLaw);
-            libraryInformation.addLibrary(libMusic);
-            libNorlin.addMedia(new Book("trees", "Norlin", "Me","ABC345","McGraw"));
-            Media m1 = new Book("trees", "Engineering", "Me","ABC345","McGraw");
-            libEngineering.addMedia(m1);
+//             Library libNorlin = new Library("Norlin");
+//             Library libBusiness = new Library("Business");
+//             Library libEngineering = new Library("Engineering");
+//             Library libLaw = new Library("Law");
+//             Library libMusic = new Library("Music");
+//             libraryInformation.addLibrary(libNorlin);
+//             libraryInformation.addLibrary(libBusiness);
+//             libraryInformation.addLibrary(libEngineering);
+//             libraryInformation.addLibrary(libLaw);
+//             libraryInformation.addLibrary(libMusic);
+//             libNorlin.addMedia(new Book("trees", "Norlin", "Me","ABC345","McGraw"));
+//             Media m1 = new Book("trees", "Engineering", "Me","ABC345","McGraw");
+//             libEngineering.addMedia(m1);
         
 
-            Reader r1 = new Reader("Chris Scarola", "scarolac@colorado.edu");
-            r1.setFees(23.13);
-            // r1.checkOutMedia(m1);
-            // m1.checkOut(r1);
-            libraryInformation.addReader(r1);
+//             Reader r1 = new Reader("Chris Scarola", "scarolac@colorado.edu");
+//             r1.setFees(23.13);
+//             // r1.checkOutMedia(m1);
+//             // m1.checkOut(r1);
+//             libraryInformation.addReader(r1);
             
 
 
@@ -127,14 +127,14 @@ public class AppTest extends TApplication
 
 
 
-        } 
-        catch (Throwable t) 
-        {
-            t.printStackTrace();
-        }
+//         } 
+//         catch (Throwable t) 
+//         {
+//             t.printStackTrace();
+//         }
         
         
 
 
-    }
-}
+//     }
+// }
